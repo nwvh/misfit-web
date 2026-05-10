5c99b5b9743cc308d0fa7522f4299d4e4a6bd1b6
wx <dominik.sladecek07@gmail.com>
2026-05-10 22:12:25 +0200

    tweak(astro.config.mjs): disable checkOrigin

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+  security: {
+    checkOrigin: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
